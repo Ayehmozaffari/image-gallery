fc0fc4432ccd040afb62817ab93549cadeffd0a3
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-07-17 19:33:10 +0430

    applying some changes
    we changed some codes

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         بچرخ تا بچرخیم
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 style={{textAlign:'center'}}>anime gallery</h1>
+    <h2 style={{textAlign:'center'}}>enjoy from our pictures</h2>
+    </>
   );
 }
 

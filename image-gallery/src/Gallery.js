ef8fed34a1f96d8f8d048406c7716b498283f72a
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-07-17 20:52:26 +0430

    we  transformed all of images

--- image-gallery/src/Gallery.js
+++ image-gallery/src/Gallery.js
@@ -1,5 +1,35 @@
 import React from "react";
+import Img1 from './img/img1.png';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
+import Img4 from './img/img4.webp';
+import Img5 from './img/img5.webp';
+import Img6 from './img/img6.jpg';s
+import Img7 from './img/img7.jpg';
+import Img8 from './img/img8.jpg';
+import Img9 from './img/img9.jpg';
+import Img10 from './img/img10.jpg';
+import Img11 from './img/img11.jpg';
+import Img12 from './img/img12.jpg';
 const Gallery=() =>{
+    let data=[
+        {
+            id:1,
+            imgSrc:'',
+        },
+        {
+            id:2,
+            imgSrc:'',
+        },
+        {
+            id:3,
+            imgSrc:'',
+        },
+        {
+            id:4,
+            imgSrc:'',
+        }
+    ]
     return(
         <h1>Gallery</h1>
     )

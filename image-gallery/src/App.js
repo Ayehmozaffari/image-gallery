d776b38e9ae39adea0d20e912f2ff68cbd1fabcd
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-07-17 19:36:13 +0430

    add the word "Gallery" & Gallery.js

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Gallery from './Gallery';
 function App() {
   return (
     <>
     <h1 style={{textAlign:'center'}}>anime gallery</h1>
     <h2 style={{textAlign:'center'}}>enjoy from our pictures</h2>
+    <Gallery/>
     </>
   );
 }
